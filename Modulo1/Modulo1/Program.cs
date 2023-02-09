bfe018c078d2fa71d1da73fe907c66d6578c9a50
NicolassHerreraSan <nicolas.herrera@usantoto.edu.co>
2023-02-08 19:33:23 -0500

    nombre usuario
    nom usu

--- Modulo1/Modulo1/Program.cs
+++ Modulo1/Modulo1/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Modulo1
+﻿using System.Runtime.InteropServices;
+
+namespace Modulo1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.Write("Holi ");
+            Console.WriteLine("jelou");
+            int edad = 18;
+            Console.WriteLine(edad);
+            string mensaje = "DX";
+            bool bandera = false;
+            var numero = 0;
+            int num1;
+            Console.WriteLine("Escriba su nombre");
+            string nom = Console.ReadLine();
+            Console.WriteLine("El nombre del usuario es "+nom);
+            Console.ReadKey();
+           
         }
     }
 }
---
+++ Modulo1/Modulo2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modulo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
