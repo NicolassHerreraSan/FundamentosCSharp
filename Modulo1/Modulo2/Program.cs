43cda4e32535d684413c8a08521e4c2667a60070
NicolassHerreraSan <nicolas.herrera@usantoto.edu.co>
2023-02-12 18:37:13 -0500

    tarea maximo float etc

--- Modulo1/Modulo2/Program.cs
+++ Modulo1/Modulo2/Program.cs
@@ -13,6 +13,16 @@
             float altura = 180.123456789f;//ocupa 4 bytes
             double anchura = 45.123456789;//ocupa 8 bytes y es mas usado
             decimal monto = 9.99m;//usa mas bytes y es mas preciso, se usa en contabilidades:)
+
+            float maxFloat = float.MaxValue;
+            double maxDouble = double.MaxValue;
+            decimal maxDecimal = decimal.MaxValue;
+
+            Console.WriteLine(maxFloat);
+            Console.WriteLine(maxDouble);
+            Console.WriteLine(maxDecimal);
+
+
         }
     }
 }
